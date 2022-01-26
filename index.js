80e7321d98023aa750ead3e779224ec22a068d60
zulmeika <zulmeika@gmail.com>
2022-01-26 16:15:53 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Get random number of dice
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+// Show who's the winner
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
